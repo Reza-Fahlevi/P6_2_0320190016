61c475a288bc70eb23215e3412a10b6bd049cae9
Reza-Fahlevi <mochammadfahlevi01@gmail.com>
2021-01-14 23:45:47 +0700

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace P6_2_0320190016
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="P6_2_0320190016.MvcApplication" Language="C#" %>
